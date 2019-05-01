a56f5e9dccf05ea7b6bbe75732fbf34d5085ae4a
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-05-01 16:10:08 -0400

    Agregando anotaciones faltante a modelo Usuario

--- Backend/src/main/java/cl/trackthor/model/Usuario.java
+++ Backend/src/main/java/cl/trackthor/model/Usuario.java
@@ -3,12 +3,16 @@ package cl.trackthor.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,6 +20,8 @@ import org.springframework.security.core.userdetails.UserDetails;
  *
  * @author Matías Hermosilla
  */
+@Entity
+@Table(name = "tra_usuario")
 public class Usuario implements Serializable, UserDetails {
     
     @Id
@@ -25,8 +31,15 @@ public class Usuario implements Serializable, UserDetails {
     @Column(name = "usu_nombre", unique = true)
     private String nombre;
 
-    @Column(name = "usu_contrasena")
-    private String contrasena;
+    @Column(name = "usu_password")
+    private String password;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    public Usuario() {
+        
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -36,12 +49,12 @@ public class Usuario implements Serializable, UserDetails {
     @Override
     @JsonIgnore
     public String getPassword() {
-        return this.contrasena;
+        return this.password;
     }
     
     @JsonProperty
     public void setPassword(String password) {
-        this.contrasena = password;
+        this.password = password;
     }
 
     @Override
@@ -67,6 +80,30 @@ public class Usuario implements Serializable, UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
     
 }
