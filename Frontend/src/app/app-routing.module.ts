b3761abec4af4f6bb62875117ffcbfec167ceb31
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-05-01 19:56:16 -0400

    Agregando estilo personalizado de Angular Material

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LoginGuard } from './guards/login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent,
+        canActivate: [LoginGuard]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ Frontend/src/app/guards/login.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+    
+    public canActivate(): boolean {
+        return true;
+    }
+
+}
