4c87344e7ff08ea673f605f2bef7606158931652
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-05-01 22:50:42 -0400

    Agregando menu de inicio de sesión

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,19 +8,22 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './modules/material/material.module';
 
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    MaterialModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
