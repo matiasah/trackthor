e27bf58d2186eb181dcd56eb0571ac67843fac14
Pablo Barría <p.barria02@ufromail.cl>
2019-05-05 00:45:25 -0400

    Create repositories with paging and sorting

---
+++ Backend/src/main/java/cl/trackthor/repository/ArriendoRepository.java
@@ -0,0 +1,13 @@
+package cl.trackthor.repository;
+
+import cl.trackthor.model.Arriendo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ArriendoRepository extends PagingAndSortingRepository<Arriendo, Long> {
+
+    @Override
+    Page<Arriendo> findAll(Pageable pageable);
+
+}
