4f4d464280af6e7ad3206af8b9311f9ab9b83f9d
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-05-07 12:49:19 -0400

    Corrigiendo diseño de botones del panel de inicio

--- Frontend/src/app/modules/administrador-empresa/administrador-empresa/administrador-empresa.component.ts
+++ Frontend/src/app/modules/administrador-empresa/administrador-empresa/administrador-empresa.component.ts
@@ -3,25 +3,25 @@ import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-administrador-empresa',
-  templateUrl: './administrador-empresa.component.html',
-  styleUrls: ['./administrador-empresa.component.scss']
+    selector: 'app-administrador-empresa',
+    templateUrl: './administrador-empresa.component.html',
+    styleUrls: ['./administrador-empresa.component.scss']
 })
 export class AdministradorEmpresaComponent implements OnInit {
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
+    public constructor(
+        private authService: AuthService,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    public ngOnInit() {
+    }
 
-  public onLogout() {
-    // Quitar token
-    this.authService.removeToken();
+    public onLogout() {
+        // Quitar token
+        this.authService.removeToken();
 
-    // Enviar a login
-    this.router.navigate(['']);
-  }
+        // Enviar a login
+        this.router.navigate(['']);
+    }
 }
