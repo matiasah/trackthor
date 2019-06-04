4260fd99403e79959a44a5cc7be5f321a6c7d10a
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-06-04 16:35:32 -0400

    Cambiando select de nombre de cliente a autocomplete

--- Frontend/src/app/modules/arriendos/registrar-arriendo/registrar-arriendo.component.ts
+++ Frontend/src/app/modules/arriendos/registrar-arriendo/registrar-arriendo.component.ts
@@ -7,88 +7,137 @@ import { ArriendoService } from 'src/app/services/arriendo.service';
 import { MatDialogRef, MatSnackBar } from '@angular/material';
 import { MaquinaService } from 'src/app/services/maquina.service';
 import { ClienteService } from 'src/app/services/cliente.service';
+import { debounceTime } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-registrar-arriendo',
-  templateUrl: './registrar-arriendo.component.html',
-  styleUrls: ['./registrar-arriendo.component.scss']
+    selector: 'app-registrar-arriendo',
+    templateUrl: './registrar-arriendo.component.html',
+    styleUrls: ['./registrar-arriendo.component.scss']
 })
 export class RegistrarArriendoComponent implements OnInit {
 
-  // Arriendo a registrar
-  public arriendo: Arriendo = {} as Arriendo;
+    // Arriendo a registrar
+    public arriendo: Arriendo = {} as Arriendo;
 
-  // Máquinas
-  public maquinas: Maquina[];
+    // Máquinas
+    public maquinas: Maquina[];
 
-  // Clientes
-  public clientes: Cliente[];
+    // Clientes
+    public clientes: Cliente[] = [];
 
-  // Indicar si se encuentra registrando
-  public registrando = false;
+    // Indicar si se encuentra registrando
+    public registrando = false;
 
-  // Formulario
-  @ViewChild('form')
-  public form: NgForm;
+    // Formulario
+    @ViewChild('form')
+    public form: NgForm;
 
-  public constructor(
-    private arriendoService: ArriendoService,
-    private dialogRef: MatDialogRef<RegistrarArriendoComponent>,
-    private snackBar: MatSnackBar,
-    private maquinaService: MaquinaService,
-    private clienteService: ClienteService
-  ) {
+    public constructor(
+        private arriendoService: ArriendoService,
+        private dialogRef: MatDialogRef<RegistrarArriendoComponent>,
+        private snackBar: MatSnackBar,
+        private maquinaService: MaquinaService,
+        private clienteService: ClienteService
+    ) {
 
-  }
-
-  public ngOnInit() {
-
-    // Obtener máquinas
-    this.maquinaService.query().subscribe(
-      Response => {
-        this.maquinas = Response;
-      }
-    );
-
-    // Obtener clientes
-    this.clienteService.query().subscribe(
-      Response => {
-        this.clientes = Response;
-      }
-    );
-  }
-
-
-  public onSubmit(): void {
-    // Si el formulario es válido
-    if (this.form.valid) {
-      // Indicar que se encuentra registrando
-      this.registrando = true;
-
-      // Registrar
-      this.arriendoService.save(this.arriendo).subscribe(
-        Respose => {
-          // Indicar que no se encuentra registrando
-          this.registrando = false;
-
-          // Notificar registro exitoso
-          this.snackBar.open('El arriendo ha sido registrado', 'Aceptar', { duration: 2000 });
-
-          // Cerrar modal
-          this.dialogRef.close();
-        },
-        Error => {
-          // Indicar que no se encuentra registrando
-          this.registrando = false;
-
-          // Notificar registro erroneo
-          this.snackBar.open('No se ha podido registrar el arriendo', 'Aceptar', { duration: 2000 });
-
-          // Cerrar modal
-          this.dialogRef.close();
-        }
-      );
     }
-  }
+
+    public ngOnInit() {
+        // Obtener máquinas
+        this.maquinaService.query().subscribe(
+            Response => {
+                this.maquinas = Response;
+            }
+        );
+    }
+
+    @ViewChild('clienteForm')
+    public set clienteForm(form: NgForm) {
+        // Si se ha recibido el input
+        if (form != null) {
+            // Obtener objeto observable
+            let observable: Observable<any> | null = form.valueChanges;
+
+            // Si hay un objeto observable
+            if (observable != null) {
+
+                // Al cambiar contenido del autocomplete
+                observable
+                    // Esperar 500ms para cualquier otro cambio
+                    .pipe(debounceTime(500))
+                    // Suscribirse a valor de autocomplete
+                    .subscribe(
+                        Value => {
+
+                            // Si el nombre del cliente es nulo, el valor no es un link
+                            if (this.getNombresCliente(Value) === null) {
+
+                                // Si el valor es un string, buscar clientes con nombres similares
+                                this.clienteService.queryFirst10ByNombresContaining(Value).subscribe(
+                                    Response => {
+                                        this.clientes = Response;
+                                    }
+                                );
+
+                            }
+                        }
+                    );
+            }
+        }
+
+    }
+
+    public onSubmit(): void {
+        // Si el formulario es válido
+        if (this.form.valid) {
+            // Indicar que se encuentra registrando
+            this.registrando = true;
+
+            // Registrar
+            this.arriendoService.save(this.arriendo).subscribe(
+                Respose => {
+                    // Indicar que no se encuentra registrando
+                    this.registrando = false;
+
+                    // Notificar registro exitoso
+                    this.snackBar.open('El arriendo ha sido registrado', 'Aceptar', { duration: 2000 });
+
+                    // Cerrar modal
+                    this.dialogRef.close();
+                },
+                Error => {
+                    // Indicar que no se encuentra registrando
+                    this.registrando = false;
+
+                    // Notificar registro erroneo
+                    this.snackBar.open('No se ha podido registrar el arriendo', 'Aceptar', { duration: 2000 });
+
+                    // Cerrar modal
+                    this.dialogRef.close();
+                }
+            );
+        }
+    }
+
+    public getNombresCliente(url: string): string | null {
+        // Si se ha recibido la lista de clientes
+        if (this.clientes != null) {
+
+            // Por cada cliente
+            for (let i = 0; i < this.clientes.length; i++) {
+                // Objeto del cliente
+                let cliente: Cliente = this.clientes[i];
+
+                // Si coincide el URL
+                if (cliente._links.self.href == url && cliente.nombres) {
+
+                    // Retornar nombre del cliente
+                    return cliente.nombres;
+                }
+            }
+        }
+        return null;
+    }
 
 }
--- Frontend/src/app/services/cliente.service.ts
+++ Frontend/src/app/services/cliente.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
@@ -24,6 +24,18 @@ export class ClienteService {
 
     public query(): Observable<Cliente[]> {
         return this.http.get<Pagination>(environment.api + 'clientes')
+            .pipe(map(Response => Response._embedded.clientes));
+    }
+
+    public queryFirst10ByNombresContaining(nombres: string): Observable<Cliente[]> {
+        // Parámetros
+        let params: HttpParams = new HttpParams();
+
+        // Fijar parámetro "nombres"
+        params = params.set('nombres', nombres);
+
+        // Obtener lista de clientes
+        return this.http.get<any>(environment.api + 'clientes/search/10-nombres', { params: params })
             .pipe(map(Response => Response._embedded.clientes));
     }
 
