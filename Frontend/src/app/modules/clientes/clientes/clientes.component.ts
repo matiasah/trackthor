38600db99ee64d6b3f7907570b7a4a8cbc780ee6
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-06-04 10:27:07 -0400

    Agregando componente de clientes

--- Frontend/src/app/modules/clientes/clientes/clientes.component.ts
+++ Frontend/src/app/modules/clientes/clientes/clientes.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatPaginator, MatTableDataSource, MatDialog, MatDialogRef } from '@angular/material';
+import { Observable } from 'rxjs';
+import { Paginator } from 'src/app/models/paginator';
+import { Page } from 'src/app/models/page';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { Cliente } from 'src/app/models/cliente';
 
 @Component({
     selector: 'app-clientes',
@@ -7,9 +13,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-    constructor() { }
+    // Columnas de datatable
+    public displayedColumns: string[] = ['nombres', 'rut', 'telefono', 'fecha_registro', 'edit', 'delete'];
 
-    ngOnInit() {
+    // Paginación
+    public paginator: Paginator<Cliente>;
+
+    // Data-source
+    public dataSource: MatTableDataSource<Cliente> = new MatTableDataSource();
+
+    // Indicar si se encuentra cargando resultados
+    public isLoading: Observable<boolean>;
+
+    // Pagina actual
+    public page: Observable<Page>;
+
+    // Sort
+    @ViewChild(MatSort)
+    public matSort: MatSort;
+
+    // Paginación
+    @ViewChild('paginator')
+    public matPaginator: MatPaginator;
+
+    public constructor(
+        private clienteService: ClienteService,
+        private dialog: MatDialog
+    ) {
+        // Instanciar paginador
+        this.paginator = this.clienteService.getPaginator();
+
+        // Observables
+        this.isLoading = this.paginator.isLoadingSubject;
+        this.page = this.paginator.pageSubject;
+    }
+
+    public ngOnInit() {
+        this.paginator.init(this.dataSource, this.matPaginator, this.matSort);
+    }
+
+    public registrar() {
+        /*
+        // Abrir dialogo
+        const ref: MatDialogRef<RegistrarEmpresaComponent> = this.dialog.open(RegistrarEmpresaComponent, {
+            width: '1000px'
+        });
+
+        // Al cerrar dialogo
+        ref.afterClosed().subscribe(
+            response => {
+                // Actualizar paginador
+                this.paginator.update();
+            }
+        );
+        */
+    }
+
+    public eliminar(cliente: Cliente) {
+        /*
+        // Crear dialogo
+        const ref: MatDialogRef<EliminarEmpresaComponent> = this.dialog.open(EliminarEmpresaComponent, {
+            width: '1000px',
+            data: empresa
+        });
+
+        // Al cerrar dialogo
+        ref.afterClosed().subscribe(
+            response => {
+                // Actualizar paginador
+                this.paginator.update();
+            }
+        );
+        */
     }
 
 }
