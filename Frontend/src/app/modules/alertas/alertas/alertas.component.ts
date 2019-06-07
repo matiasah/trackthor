d63405ab8a044a87654affe426c8fc520499d6fe
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-06-07 12:28:52 -0400

    Permitir visualizar alertas

---
+++ Frontend/src/app/modules/alertas/alertas/alertas.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Paginator } from 'src/app/models/paginator';
+import { MatTableDataSource, MatSort, MatPaginator, MatDialog, MatDialogRef } from '@angular/material';
+import { Observable } from 'rxjs';
+import { Page } from 'src/app/models/page';
+import { AlertaService } from 'src/app/services/alerta.service';
+import { Alerta } from 'src/app/models/alerta';
+
+@Component({
+    selector: 'app-alertas',
+    templateUrl: './alertas.component.html',
+    styleUrls: ['./alertas.component.scss']
+})
+export class AlertasComponent implements OnInit {
+
+    // Columnas de datatable
+    public displayedColumns: string[] = ['descripcion', 'empresa', 'usuario', 'fecha_creacion', 'delete'];
+
+    // Paginación
+    public paginator: Paginator<Alerta>;
+
+    // Data-source
+    public dataSource: MatTableDataSource<Alerta> = new MatTableDataSource();
+
+    // Indicar si se encuentra cargando resultados
+    public isLoading: Observable<boolean>;
+
+    // Pagina actual
+    public page: Observable<Page>;
+
+    // Sort
+    @ViewChild(MatSort)
+    public matSort: MatSort;
+
+    // Paginación
+    @ViewChild('paginator')
+    public matPaginator: MatPaginator;
+
+    public constructor(
+        private alertaService: AlertaService,
+        private dialog: MatDialog
+    ) {
+        // Instanciar paginador
+        this.paginator = this.alertaService.getPaginator();
+
+        // Observables
+        this.isLoading = this.paginator.isLoadingSubject;
+        this.page = this.paginator.pageSubject;
+    }
+
+    public ngOnInit() {
+        this.paginator.init(this.dataSource, this.matPaginator, this.matSort);
+    }
+
+    /*
+    public eliminar(alerta: Alerta) {
+        // Crear dialogo
+        const ref: MatDialogRef<EliminarAlertaComponent> = this.dialog.open(EliminarAlertaComponent, {
+            width: '1000px',
+            data: alerta
+        });
+
+        // Al cerrar dialogo
+        ref.afterClosed().subscribe(
+            response => {
+                // Actualizar paginador
+                this.paginator.update();
+            }
+        );
+    }
+    */
+
+}
--- Frontend/src/app/services/arriendo.service.ts
+++ Frontend/src/app/services/arriendo.service.ts
@@ -1,43 +1,45 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Paginator } from '../models/paginator';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Arriendo } from '../models/arriendo';
-import { Pagination } from '../models/pagination';
-import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { Pagination } from '../models/pagination';
+import { Paginator } from '../models/paginator';
+import { Arriendo } from '../models/arriendo';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ArriendoService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+    public constructor(
+        private http: HttpClient
+    ) {
 
-  public getPaginator(): Paginator<any> {
-    return new Paginator(this.http, 'arriendos/search/page', 'arriendos');
-  }
+    }
 
-  public query(): Observable<Arriendo[]> {
-    return this.http.get<Pagination>(environment.api + 'arriendos')
-      .pipe(map(Response => Response._embedded.arriendos));
-  }
+    public getPaginator(): Paginator<any> {
+        return new Paginator(this.http, 'arriendos/search/page', 'arriendos');
+    }
 
-  public get(path: string): Observable<Arriendo> {
-    return this.http.get<Arriendo>(path);
-  }
+    public query(): Observable<Arriendo[]> {
+        return this.http.get<Pagination>(environment.api + 'arriendos')
+            .pipe(map(Response => Response._embedded.arriendos));
+    }
 
-  public save(arriendo: Arriendo): Observable<any> {
-    return this.http.post(environment.api + 'arriendos', arriendo);
-  }
+    public get(path: string): Observable<Arriendo> {
+        return this.http.get<Arriendo>(path);
+    }
 
-  public update(arriendo: Arriendo): Observable<any> {
-    return this.http.patch(arriendo._links.self.href, arriendo);
-  }
+    public save(arriendo: Arriendo): Observable<any> {
+        return this.http.post(environment.api + 'arriendos', arriendo);
+    }
 
-  public delete(arriendo: Arriendo): Observable<any> {
-    return this.http.delete(arriendo._links.self.href);
-  }
+    public update(arriendo: Arriendo): Observable<any> {
+        return this.http.patch(arriendo._links.self.href, arriendo);
+    }
+
+    public delete(arriendo: Arriendo): Observable<any> {
+        return this.http.delete(arriendo._links.self.href);
+    }
 }
