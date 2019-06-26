be2f791257952cc37eab5199c1bf155e3204d62d
Felipe <f.quezada01@ufromail.cl>
2019-06-26 17:57:16 -0400

    Se corrige comando iniciar Frontend y acceder desde Internet.

--- Frontend/start.sh
+++ Frontend/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-npm install && ng serve --host 0.0.0.0 --poll 500
+#npm install && ng serve --host 0.0.0.0 --poll 500
+npm install && ng serve --host 0.0.0.0 --poll 500 --disable-host-check
